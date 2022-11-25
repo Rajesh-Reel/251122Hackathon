b3e095fa824d53baf11656d4f8f2196f5090d11e
Domus Aurea <philip.doge.kaminski@gmail.com>
2022-11-25 14:55:38 +0000

    emails send!

--- 251122Hackathon/backendFolder/index.js
+++ 251122Hackathon/backendFolder/index.js
@@ -1,15 +1,39 @@
 //
 import express from "express";
 const app = express();
-import cors from 'cors'
+import cors from "cors";
+import axios from "axios";
+import * as dotenv from "dotenv";
+dotenv.config();
 app.use(express.json());
 
-app.use(cors())
-
-app.post('/', (req, res) => {
+app.use(cors());
+console.log(process.env.SENDER_EMAIL, process.env.EMAIL_KEY);
+app.post("/", (req, res) => {
   console.log(req.body);
-  res.json(req.body)
-})  
+  res.json(req.body);
+
+  const options = {
+    method: "POST",
+    url: "https://rapidprod-sendgrid-v1.p.rapidapi.com/mail/send",
+    headers: {
+      "content-type": "application/json",
+      "X-RapidAPI-Key": `${process.env.EMAIL_KEY}`,
+      "X-RapidAPI-Host": "rapidprod-sendgrid-v1.p.rapidapi.com",
+    },
+    data: `{"personalizations":[{"to":[{"email":"${req.body.email}"}],"subject":"Yooooo!"}],"from":{"email":"from_philip.doge.kaminski@gmail.com"},"content":[{"type":"text/plain","value":"Hello, World!"}]}`,
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+      console.log(process.env.SENDER_EMAIL);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+});
 
 // app.post('/', (req, res) => {
 //   res.send(req)
@@ -17,7 +41,4 @@ app.post('/', (req, res) => {
 
 const PORT = 3000;
 
-
-
-
-app.listen(PORT, console.log(`Listening on Port ${PORT}`))
+app.listen(PORT, console.log(`Listening on Port ${PORT}`));
