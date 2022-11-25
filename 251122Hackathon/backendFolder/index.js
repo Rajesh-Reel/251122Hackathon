06b522e2672fe59511862838e664241f71c4ee6d
Lorentz <91217473+Lauro235@users.noreply.github.com>
2022-11-25 10:01:34 +0000

    all working starting point

--- 251122Hackathon/backendFolder/index.js
+++ 251122Hackathon/backendFolder/index.js
@@ -4,5 +4,5 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-// console.log('Welcome to the back end on ${PORT}')
-app.listen(PORT, function() {console.log('Listening on Port ${PORT}')})
+
+app.listen(PORT, console.log(`Listening on Port ${PORT}`))
