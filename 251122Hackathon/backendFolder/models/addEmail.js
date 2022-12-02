28483ff66cb34b39772ab080b51b769d262b39a8
Domus Aurea <philip.doge.kaminski@gmail.com>
2022-12-02 14:43:58 +0000

    backend struggle continues

--- 251122Hackathon/backendFolder/models/addEmail.js
+++ 251122Hackathon/backendFolder/models/addEmail.js
@@ -1,7 +1,11 @@
+import { pool, query } from "../db/index.js";
+
 export async function addEmail() {
   const email = await query(
-    `INSERT INTO emails (content) VALUES $1 RETURNING *;`,
-    ["ph.kaminski.dev@gmail.com"]
+    `INSERT INTO emails (content) VALUES ('example@gmail.com') RETURNING *;`
   );
   return email.rows;
 }
+
+// `INSERT INTO emails (content) VALUES (
+//     'ph.kaminski.dev@gmail.com') RETURNING *;`
