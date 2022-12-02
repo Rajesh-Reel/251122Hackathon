c2e541f209727f5c83b737549d54c83a78067c71
Domus Aurea <philip.doge.kaminski@gmail.com>
2022-12-02 10:51:49 +0000

    tested that the request works with user-submitted email, subject and message

--- 251122Hackathon/backendFolder/index.js
+++ 251122Hackathon/backendFolder/index.js
@@ -15,26 +15,26 @@ app.post("/", (req, res) => {
   console.log(req.body);
   res.json(req.body);
 
-  // const options = {
-  //   method: "POST",
-  //   url: "https://rapidprod-sendgrid-v1.p.rapidapi.com/mail/send",
-  //   headers: {
-  //     "content-type": "application/json",
-  //     "X-RapidAPI-Key": `${process.env.EMAIL_KEY}`,
-  //     "X-RapidAPI-Host": "rapidprod-sendgrid-v1.p.rapidapi.com",
-  //   },
-  //   data: `{"personalizations":[{"to":[{"email":"${req.body.email}"}],"subject":"Yooooo!"}],"from":{"email":"from_philip.doge.kaminski@gmail.com"},"content":[{"type":"text/plain","value":"Hello, World!"}]}`,
-  // };
+  const options = {
+    method: "POST",
+    url: "https://rapidprod-sendgrid-v1.p.rapidapi.com/mail/send",
+    headers: {
+      "content-type": "application/json",
+      "X-RapidAPI-Key": `${process.env.EMAIL_KEY}`,
+      "X-RapidAPI-Host": "rapidprod-sendgrid-v1.p.rapidapi.com",
+    },
+    data: `{"personalizations":[{"to":[{"email":"${req.body.email}"}],"subject":"${req.body.subject}"}],"from":{"email":"from_philip.doge.kaminski@gmail.com"},"content":[{"type":"text/plain","value":"${req.body.message}"}]}`,
+  };
 
-  // axios
-  //   .request(options)
-  //   .then(function (response) {
-  //     console.log(response.data);
-  //     console.log(process.env.SENDER_EMAIL);
-  //   })
-  //   .catch(function (error) {
-  //     console.error(error);
-  //   });
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+      console.log(process.env.SENDER_EMAIL);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
 });
 
 const PORT = 3000;
