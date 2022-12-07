a912cd7a9e1572c7b7ac03c0841e137700dfa377
Domus Aurea <philip.doge.kaminski@gmail.com>
2022-12-07 09:57:23 +0000

    get and post requests working

--- 251122Hackathon/backendFolder/models/addEmail.js
+++ 251122Hackathon/backendFolder/models/addEmail.js
@@ -1,11 +1,16 @@
 import { pool, query } from "../db/index.js";
 
-export async function addEmail() {
+export async function addEmail(newEmail) {
   const email = await query(
-    `INSERT INTO emails (content) VALUES ('example@gmail.com') RETURNING *;`
+    `INSERT INTO emails (content) VALUES ($1) RETURNING *;`,
+    [newEmail.email]
   );
-  return email.rows;
+  return email.rows[0];
 }
 
+export async function getEmail() {
+  const email = await query(`SELECT * FROM emails`);
+  return email;
+}
 // `INSERT INTO emails (content) VALUES (
 //     'ph.kaminski.dev@gmail.com') RETURNING *;`
--- 251122Hackathon/backendFolder/routes/index.js
+++ 251122Hackathon/backendFolder/routes/index.js
@@ -1,11 +1,25 @@
 import express from "express";
 import sendEmail from "../models/sendEmail.js";
-import { addEmail } from "../models/addEmail.js";
+import { addEmail, getEmail } from "../models/addEmail.js";
 
 const router = express.Router();
 
-router.post("/", addEmail);
+// router.post("/", addEmail);
+router.post("/", async function (req, res) {
+  const newEmail = req.body;
+  const result = await addEmail(newEmail);
 
+  res.json({
+    success: true,
+    payload: result,
+  });
+});
+
+router.get("/", async function (req, res) {
+  const result = await getEmail();
+  //what difference would it make if we replaced res.json() with res.send() here?
+  res.json(result);
+});
 // router.post("/", sendEmail);
 
 export default router;
