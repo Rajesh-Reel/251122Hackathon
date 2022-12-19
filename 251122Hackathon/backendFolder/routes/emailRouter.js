aefcb3ef4a8451c747624475a69a5e8cd27b028b
Domus Aurea <philip.doge.kaminski@gmail.com>
2022-12-19 09:22:59 +0000

    post request for the API added to EmailRouter

--- 251122Hackathon/backendFolder/routes/emailRouter.js
+++ 251122Hackathon/backendFolder/routes/emailRouter.js
@@ -1,11 +1,9 @@
 import express from "express";
 import sendEmail from "../models/sendEmail.js";
 // import { addEmail, getEmail, deleteEmail } from "../models/EmailModels.js";
-import getEmail from '../db/scripts/getEmail.js'
-import deleteEmail from '../db/scripts/deleteEmail.js'
-import addEmail from '../db/scripts/addEmail.js'
-
-
+import getEmail from "../db/scripts/getEmail.js";
+import deleteEmail from "../db/scripts/deleteEmail.js";
+import addEmail from "../db/scripts/addEmail.js";
 
 const router = express.Router();
 
@@ -35,6 +33,29 @@ router.delete("/:id", async function (req, res) {
     success: true,
     payload: result,
   });
+});
+
+// router.get("/send", async function (req, res) {
+//   const result = await getEmail();
+
+//   console.log(result.rows);
+//   res.json(result.rows);
+// });
+
+router.post("/send", (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
+
+  const options = {
+    method: "POST",
+    url: "https://rapidprod-sendgrid-v1.p.rapidapi.com/mail/send",
+    headers: {
+      "content-type": "application/json",
+      "X-RapidAPI-Key": `${process.env.EMAIL_KEY}`,
+      "X-RapidAPI-Host": "rapidprod-sendgrid-v1.p.rapidapi.com",
+    },
+    data: `{"personalizations":[{"to":[{"email":"${req.body.email}"}],"subject":"Yooooo!"}],"from":{"email":"from_${process.env.SENDER_EMAIL}"},"content":[{"type":"text/plain","value":"Hello, World!"}]}`,
+  };
 });
 
 export default router;
